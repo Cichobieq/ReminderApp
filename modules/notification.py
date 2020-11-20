f5de02411e6a649ff999d6b2231791898278c904
aw0s <michal.sowa200000@gmail.com>
2020-11-20 11:35:29 +0100

    Notifications work

--- modules/notification.py
+++ modules/notification.py
@@ -4,29 +4,37 @@ import json
 
 import pynotifier as pn
 
+
 def notify_fn():
-    with open(r"data/all_reminds.json", "r+") as ar:
-        all_reminds = json.load(ar)["data"]
+    while True:
+        with open(r"data/all_reminds.json", "r+") as ar:
+            all_reminds = json.load(ar)["data"]
 
-    current_date_and_time = datetime.datetime.now()
-    current_time = f"{current_date_and_time.hour}:{current_date_and_time.minute}"
-    current_date = f"{current_date_and_time.day}/{current_date_and_time.month}/{current_date_and_time.year}"
+        current_date_and_time = datetime.datetime.now()
+        current_time = f"{current_date_and_time.hour}:{current_date_and_time.minute}:{current_date_and_time.second}"
+        current_date = f"{current_date_and_time.day}/{current_date_and_time.month}/{current_date_and_time.year}"
 
-    for element in all_reminds:
-        if (
-                element["date"] == current_date
-                and element["time"] == current_time
-                and f"{current_time}:0" == f"{current_time}:{current_date_and_time.second}"
-        ):
-            if element["notification"]:
-                remind_notification = pn.Notification(
-                    title=element["title"],
-                    description=element["description"],
-                    icon_path=r"data/img/notification-clock-icon.png",
-                    duration=5,
-                    urgency=pn.Notification.URGENCY_NORMAL,
-                )
-                remind_notification.send()
+        print(current_time)
 
-                break
-    time.sleep(1)
+        for element in all_reminds:
+            print(element["date"])
+            print(current_date)
+            print(current_time)
+            print(f"{element['time']}:0")
+
+            if (
+                    element["date"] == current_date
+                    and current_time == f"{element['time']}:0"
+            ):
+                if element["notification"]:
+                    remind_notification = pn.Notification(
+                        title=element["title"],
+                        description=element["description"],
+                        icon_path=r"data/img/notification-clock-icon.png",
+                        duration=5,
+                        urgency=pn.Notification.URGENCY_NORMAL,
+                    )
+                    remind_notification.send()
+
+                    break
+        time.sleep(1)
